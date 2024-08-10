d6e90b26c8a4780ed3242e86bf0ce5d556280587
Zekeriya <Zekeriya@ZEKERIYA>
2024-08-10 15:32:18 +0300

    Add project files.

---
+++ PratikLists1/Program.cs
@@ -0,0 +1,16 @@
+﻿List<string> guests = new List<string>
+{
+            "Bülent Ersoy",
+            "Ajda Pekkan",
+            "Ebru Gündeş",
+            "Hadise",
+            "Hande Yener",
+            "Tarkan",
+            "Funda Arar",
+            "Demet Akalın"
+};
+
+foreach(string guest in guests)
+{
+    Console.WriteLine(guest);
+}
